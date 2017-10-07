84f851f4c2a4f61f3c1a1ec7ffa3982e8dab6280
mozhavro <mozhario@gmail.com>
2017-10-07 13:52:08 +0300

    Provided adding user to a default group on registration

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,9 +1,16 @@
 from registration.forms import RegistrationForm
+from django.contrib.auth.models import Group
 
 from .models import User
 
 
 class UserForm(RegistrationForm):
+    '''
+    User register form overriden to add some custom
+    user model fields and add user to default group 
+    when saving
+    '''
+
     class Meta:
         model = User
         fields = (
@@ -12,3 +19,14 @@ class UserForm(RegistrationForm):
         	'username',
         	'email',
         )
+
+    def save(self, commit=True, *args, **kwargs):
+        user = super(UserForm, self).save(commit=False, *args, **kwargs)
+
+        if commit:
+            group = Group.objects.get(name='User')
+            # FIXME: doesn't create a relation for some reason
+            user.groups.add(group)
+            user.save()
+
+        return user
