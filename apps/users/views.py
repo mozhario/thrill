0f846b8a6a93f3a008581fd5d63a4a343be27dd1
mozhario <mozharov.atwork@gmail.com>
2017-10-09 17:01:52 +0300

    Added profile redirect view

--- apps/users/views.py
+++ apps/users/views.py
@@ -69,6 +69,9 @@ class UserEditView(LoginRequiredMixin, UpdateView):
         user = self.request.user
         return user
 
-    # def form_valid(self, form):
 
-    #     return super(UserEditView, self).form_valid(form)
+class ProfileView(LoginRequiredMixin, View):
+    login_url = 'auth_login'
+
+    def get(self, request):
+        return redirect('user_detail', request.user.username)
