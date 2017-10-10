bd78ec7ea0eb27f26736c2cf5d86afd41983258c
mozhario <mozharov.atwork@gmail.com>
2017-10-10 11:54:28 +0300

    Moved abstract post model to base package

--- apps/base/models.py
+++ apps/base/models.py
@@ -7,3 +7,13 @@ class Timestamps(models.Model):
 
 	class Meta:
 		abstract = True
+
+
+class Post(Timestamps):
+	title = models.CharField(max_length=140)
+	content = models.CharField(max_length=9999)
+	excerpt = models.CharField(max_length=300, blank=True)
+	thumbnail = models.ImageField(blank=True)
+
+	class Meta:
+		abstract = True
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -4,17 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from apps.users.models import User
 from apps.communities.models import Community
-from apps.base.models import Timestamps
-
-
-class Post(Timestamps):
-	title = models.CharField(max_length=140)
-	content = models.CharField(max_length=9999)
-	excerpt = models.CharField(max_length=300, blank=True)
-	thumbnail = models.ImageField(blank=True)
-
-	class Meta:
-		abstract = True
+from apps.base.models import Post
 
 
 class UserPost(Post):
