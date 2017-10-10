e14989722839ffc14483ba4ae01f8198f363d783
mozhario <mozharov.atwork@gmail.com>
2017-10-10 11:56:34 +0300

    Fixed unresolved dependencies

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 
 from apps.users.models import User
 from apps.communities.models import Community
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class User(AbstractUser):
@@ -20,6 +22,7 @@ class UserSubscription(models.Model):
 	user = models.ForeignKey(User)
 	created_at = models.DateTimeField(auto_now_add=True)
 
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+	content_type = models.ForeignKey(ContentType)
+	object_id = models.PositiveIntegerField()
+	content_object = GenericForeignKey('content_type', 'object_id')
+	
