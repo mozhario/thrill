7990cffae3b55b3bd826774dc78294a520816544
mozhario <mozharov.atwork@gmail.com>
2017-10-10 11:53:27 +0300

    Added base models package. Moved timestamps to base abstract model

--- apps/communities/models.py
+++ apps/communities/models.py
@@ -1,11 +1,9 @@
 from django.db import models
+from apps.base.models import Timestamps
 
 
-class Community(models.Model):
+class Community(Timestamps):
 	title = models.CharField(max_length=140)
 	short_link = models.CharField(max_length=40, unique=True)
 	description = models.CharField(max_length=9999)
 	avatar_pic = models.ImageField(blank=True)
-
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -4,16 +4,14 @@ from django.contrib.contenttypes.models import ContentType
 
 from apps.users.models import User
 from apps.communities.models import Community
+from apps.base.models import Timestamps
 
 
-class Post(models.Model):
+class Post(Timestamps):
 	title = models.CharField(max_length=140)
 	content = models.CharField(max_length=9999)
 	excerpt = models.CharField(max_length=300, blank=True)
 	thumbnail = models.ImageField(blank=True)
-
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		abstract = True
