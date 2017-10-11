d444aaf5b9b394a40513b1c233df6c5ba62cdcdb
mozhario <mozharov.atwork@gmail.com>
2017-10-11 17:34:57 +0300

    Users subscribers/subscriptions (just prototyped for now, in process)

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,6 +4,13 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 from django.contrib.contenttypes.models import ContentType
 
 
+class UserManager(models.Manager):
+    pass
+    # def get_queryset(self):
+    #     TODO: get subscriptions and subscribers
+    #     pass
+
+
 class User(AbstractUser):
     '''
     Custom user model that adds some account profile fieds
@@ -14,6 +21,8 @@ class User(AbstractUser):
     phone = models.CharField(max_length=12, blank=True)
     profile_pic = models.ImageField(blank=True)
     subscriptions = GenericRelation('UserSubscription')
+
+    objects = UserManager()
 
     def subscribe(self, obj):
         self.subscriptions.create(user_id=self.pk, content_object=obj)
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^users/$', UserList.as_view(), name='user_list'),
     url(r'^users/(?P<username>[\w.@+-]+)/$', UserDetail.as_view(), name='user_detail'),
     url(r'^users/subscribe/(?P<user_id>\d+)/$', SubscribeToUser.as_view(), name='user_subscribe'),
+    
     url(r'^settings/$', UserEditView.as_view(), name='user_edit'),
     url(r'profile/$', ProfileView.as_view(), name="user_profile")
     # url(r'^settings/delete-account$', UserDeleteView.as_view(), name='user_settings_delete'),
