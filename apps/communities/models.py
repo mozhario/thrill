5f055d415dfc72ba225f4532f601d264a646adf5
mozhario <mozharov.atwork@gmail.com>
2017-10-13 17:24:48 +0300

    Changed community description to textfield

--- apps/communities/models.py
+++ apps/communities/models.py
@@ -7,7 +7,7 @@ from apps.users.models import User
 class Community(Timestamps):
     title = models.CharField(max_length=140)
     short_link = models.CharField(max_length=40, unique=True, null=True, blank=True) # TODO validate as username (maybe property)
-    description = models.CharField(max_length=9999)
+    description = models.TextField(max_length=9999)
     avatar_pic = models.ImageField(blank=True)
     admin = models.ForeignKey(User)
 
