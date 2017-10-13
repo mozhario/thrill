0f1df150b117dccf8ae0c9a2b037ca8c33e2a39a
mozhario <mozharov.atwork@gmail.com>
2017-10-13 15:15:26 +0300

    Added community admin validation on community post create view (needs refactoring)

--- apps/communities/views.py
+++ apps/communities/views.py
@@ -3,7 +3,7 @@ from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
+from django.http import Http404, HttpResponseForbidden
 
 from apps.base.views import PostCreateView
 from .models import Community, CommunityPost
@@ -48,8 +48,25 @@ class CommunityCreateView(LoginRequiredMixin, CreateView):
 
 
 class CommunityPostCreateView(PostCreateView):
+    # TODO refactoring for:
+    # 1. getting community object by url param in more generic way
+    # 2. checking if user is admin of community should be a mixin or
     model = CommunityPost
     template_name = 'posts/post_form.html'
+
+    def get(self, request, *args, **kwargs):
+        response = super(CommunityPostCreateView, self).get(request, *args, **kwargs)
+        short_link = self.kwargs.get('short_link', None)
+        pk = self.kwargs.get('pk', None)
+        if short_link:
+            community = Community.objects.get(short_link=short_link)
+        elif pk:
+            community = Community.objects.get(pk=pk)
+
+        if community.admin != request.user:
+            return HttpResponseForbidden("You are not allowed to manage this community.")
+
+        return response
 
     def form_valid(self, form):
         post = form.save(commit=False)
