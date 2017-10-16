63c807183b19c4d9e14ab12186f8a010407db8e9
mozhario <mozharov.atwork@gmail.com>
2017-10-16 15:25:24 +0300

    Implemented an  update of subscriptions cache on user subscribe/unsubscribe action

--- apps/users/services.py
+++ apps/users/services.py
@@ -1,9 +1,34 @@
 from .models import UserSubscription
+from django.core.cache import cache
+
 
 class UserUserSubscriptionManager():
     '''
     Service that manages user-to-user subscription/unsubscription
     '''
+    @staticmethod
+    def _update_caches(user, second_user):
+        '''
+        user - is user who (subscribes to / unsubscribes from) second_user.
+        This method updates caches of user subscriptions and second users
+        subscribers 
+        '''
+        user_subscribed_to = [subscription.content_object for subscription 
+                                in UserSubscription.objects.filter(user_id=user.pk)] 
+
+        second_user_subscribers = [subscription.user for subscription 
+                                    in UserSubscription.objects.filter(object_id=second_user.pk)]
+
+        cache.set(
+            'user_{id}_subscribed_to'.format(id=user.pk),
+            user_subscribed_to
+        )
+
+        cache.set(
+            'user_{id}_subscribers'.format(id=second_user.pk), 
+            second_user_subscribers
+        )
+
     @staticmethod
     def subscribe(user, user_to_subscribe):
         '''
@@ -11,6 +36,7 @@ class UserUserSubscriptionManager():
         Subscribes a user to user_to_subscribe.
         '''
         user.subscriptions.create(user_id=user.pk, content_object=user_to_subscribe)
+        UserUserSubscriptionManager._update_caches(user, user_to_subscribe)
 
     @staticmethod
     def unsubscribe(user, user_to_unsubscribe):
@@ -20,6 +46,7 @@ class UserUserSubscriptionManager():
         '''
         subscription = UserSubscription.objects.filter(user=user, object_id=user_to_unsubscribe.id)
         subscription.delete()
+        UserUserSubscriptionManager._update_caches(user, user_to_unsubscribe)
 
 
 class UserCommunitySubscriptionManager():
