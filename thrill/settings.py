bebff29e8a527a55a7011ca251352a121e5e0843
mozhario <mozharov.atwork@gmail.com>
2017-10-18 17:14:02 +0300

    Integrated js markdown editor

--- thrill/settings.py
+++ thrill/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR
+# STATIC_ROOT = BASE_DIR
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
