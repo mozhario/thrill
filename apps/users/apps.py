ecc4938415e54258cb42b2f3747ff78eeadcf7f0
mozhario <mozharov.atwork@gmail.com>
2017-10-18 13:37:58 +0300

    fixed appconfig inclusion

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -2,11 +2,11 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    name = 'users'
+    name = 'apps.users'
 
     def ready(self):
         from actstream import registry
         registry.register(self.get_model('User'))
         registry.register(self.get_model('UserPost'))
 
-        import .signals.handlers
+        import apps.users.signals.handlers
