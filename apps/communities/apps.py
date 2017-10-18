7af0b814cc84d26b9a3dc411adc8e273b87c1b13
mozhario <mozharov.atwork@gmail.com>
2017-10-18 14:11:24 +0300

    Fixed communities AppConfig inclusion

--- apps/communities/apps.py
+++ apps/communities/apps.py
@@ -2,11 +2,11 @@ from django.apps import AppConfig
 
 
 class CommunitiesConfig(AppConfig):
-    name = 'communities'
+    name = 'apps.communities'
 
     def ready(self):
         from actstream import registry
         registry.register(self.get_model('Community'))
         registry.register(self.get_model('CommunityPost'))
 
-        import .signals.handlers
+        import apps.users.signals.handlers
