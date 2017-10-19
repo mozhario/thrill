1323b5771f021c920020fa975fbdb75c877786b8
mozhario <mozharov.atwork@gmail.com>
2017-10-19 17:02:11 +0300

    Made comments a tree structure (using django mptt)

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
+from mptt.models import MPTTModel, TreeForeignKey
+
 # from apps.users.models import User
 from apps.base.models import Timestamps
 
 
-class Comment(Timestamps):
+class Comment(MPTTModel, Timestamps):
     author = models.ForeignKey('users.User')
     content = models.TextField(max_length=240)
+
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
 
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
