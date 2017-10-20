6eeec046c2b7133d25d481c9f7bd440097105407
mozhario <mozharov.atwork@gmail.com>
2017-10-20 12:58:42 +0300

    implemented chat models

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from apps.base.models import Timestamps
 
-# Create your models here.
+
+class Message(Timestamps):
+    user = models.ForeignKey('users.User')
+    text = models.TextField(max_length=3000)
+    room = models.ForeignKey('chat.Room')
+
+
+class Room(Timestamps):
+    title = models.CharField(max_length=100)
+    users = models.ManyToManyField('users.User')
