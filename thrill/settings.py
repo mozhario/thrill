4bda685eff118a6d8b2c733f953c473d77e8efa6
mozhario <mozharov.atwork@gmail.com>
2017-10-25 11:50:07 +0300

    Separated cache and sessions redis storages

--- thrill/settings.py
+++ thrill/settings.py
@@ -156,10 +156,19 @@ CACHES = {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
         "KEY_PREFIX": "rcahce"
+    },
+    "sessions": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "sess"
     }
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+SESSION_CACHE_ALIAS = "sessions"
 
 # CELERY_IMPORTS = ("tasks", )
 
