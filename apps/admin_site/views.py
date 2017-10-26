b3f8b23f5da4f2d4f8f4c75d74edeb54f4959b95
mozhario <mozharov.atwork@gmail.com>
2017-10-26 12:03:19 +0300

    Fixed users online admin view and template

--- apps/admin_site/views.py
+++ apps/admin_site/views.py
@@ -8,11 +8,11 @@ from django_redis import get_redis_connection
 def online_users(request):
     con = get_redis_connection("default")
 
-    username = re.compile(r"user_online_([a-zA-Z0-9_]*)")
+    username_pattern = re.compile(r"user_online_([a-zA-Z0-9_]*)")
 
     users = {}
     for key in con.scan_iter('user_online_*'):
-        username = username.search(key.decode('utf-8')).group(1)
+        username = username_pattern.search(key.decode('utf-8')).group(1)
         users[username] = con.get(key).decode('utf-8')
 
     return render(request, 'admin/users_online.html', context={
