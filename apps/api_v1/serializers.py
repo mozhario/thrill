87ebd8b2a10780ee38eb7497637d76551e67a661
mozhario <mozharov.atwork@gmail.com>
2017-11-01 13:04:55 +0200

    Adjusted serializers

--- apps/api_v1/serializers.py
+++ apps/api_v1/serializers.py
@@ -1,39 +1,37 @@
 from rest_framework import serializers
 
-from apps.users.models import User
+from apps.users.models import User, UserPost
+from apps.communities.models import Community, CommunityPost
 from django.contrib.auth.models import Group
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    username = serializers.CharField(max_length=100)
-    first_name = serializers.CharField(max_length=100)
-    last_name = serializers.CharField(max_length=100)
-    email = serializers.EmailField()
-    bio = serializers.CharField(max_length=500)
-    location = serializers.CharField(max_length=30)
-    birth_date = serializers.DateField()
-    phone = serializers.CharField(max_length=12)
-    profile_pic = serializers.CharField()
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
-class GroupSerializer(serializers.Serializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
 
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=140)
-    content = serializers.CharField(max_length=9999)
-    excerpt = serializers.CharField(max_length=300)
-    thumbnail = serializers.ImageField()
-    likes_count = serializers.IntegerField(default=0)
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = None
+        fields = '__all__'
 
 
 class UserPostSerializer(PostSerializer):
-    user_id = serializers.IntegerField()
+    user_id = serializers.ReadOnlyField()
+
+    class Meta(PostSerializer.Meta):
+        model = UserPost
 
 
 class CommunityPostSerializer(PostSerializer):
-    community_id = serializers.IntegerField()
+
+    class Meta(PostSerializer.Meta):
+        model = CommunityPost
