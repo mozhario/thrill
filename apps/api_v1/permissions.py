d50eabcc9a0cd42c8dd6762ed50ddf94bacf6bb8
mozhario <mozharov.atwork@gmail.com>
2017-11-01 10:38:24 +0200

    Added permissions to posts api viewsets

--- apps/api_v1/permissions.py
+++ apps/api_v1/permissions.py
@@ -2,8 +2,33 @@ from rest_framework import permissions
 
 
 class IsMeOrReadOnly(permissions.BasePermission):
+    """
+    Only allows user to edit his own account
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj == request.user
+
+
+class IsPostAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Only author of the post can edit the post
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
+
+
+class IsCommunityAdminOrReadOnly(permissions.BasePermission):
+    """
+    Only author of the post can edit the post
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.community.admin == request.user
--- apps/api_v1/views.py
+++ apps/api_v1/views.py
@@ -7,7 +7,7 @@ from apps.users.models import User, UserPost
 from apps.communities.models import  Community, CommunityPost
 from django.contrib.auth.models import Group
 from . import serializers
-from .permissions import IsMeOrReadOnly
+from .permissions import IsMeOrReadOnly, IsPostAuthorOrReadOnly, IsCommunityAdminOrReadOnly
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -28,11 +28,19 @@ class GroupViewSet(viewsets.ModelViewSet):
 
 
 class UserPostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows User Posts to be viewed or edited.
+    """
+    permission_classes = (IsPostAuthorOrReadOnly,)
     queryset = UserPost.objects.all()
     serializer_class = serializers.UserPostSerializer
 
 
 class CommunityPostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Community Posts to be viewed or edited.
+    """
+    permission_classes = (IsCommunityAdminOrReadOnly,)
     queryset = CommunityPost.objects.all()
     serializer_class = serializers.CommunityPostSerializer
 
