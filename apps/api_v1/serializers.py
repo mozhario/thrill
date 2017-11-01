7be9251b28e5b0d1278ae30512df36b401ada1c1
mozhario <mozharov.atwork@gmail.com>
2017-11-01 13:05:31 +0200

    Added validation to posts serializer

--- apps/api_v1/serializers.py
+++ apps/api_v1/serializers.py
@@ -23,6 +23,22 @@ class PostSerializer(serializers.ModelSerializer):
         model = None
         fields = '__all__'
 
+    def validate(self, attrs):
+        """
+        Post must have atleast content or thumbnail pic
+        """
+        required_fields = dict()
+        required_fields['content'] = attrs.get('content', None)
+        required_fields['thumbnail'] = attrs.get('thumbnail', None)
+        if self.instance:
+            required_fields['instance_content'] = self.instance.content
+            required_fields['instance_thumbnail'] = self.instance.content
+
+        if not any(required_fields):
+            raise serializers.ValidationError('Post should have atleast a content or thumbnail field filled.')
+
+        return attrs
+
 
 class UserPostSerializer(PostSerializer):
     user_id = serializers.ReadOnlyField()
